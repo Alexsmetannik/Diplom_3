8c2bb14a3bcdb14807f0d84216afd2ae91a0e59b
Alexandr Galkin <alexzagigalkin@yandex.ru>
2023-11-25 22:32:06 +0300

    fix error with method isDisplayed

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -3,6 +3,7 @@ package pages;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class MainPage {
     private WebDriver driver;
@@ -15,12 +16,10 @@ public class MainPage {
     private static final By MAKE_ORDER_BUTTON = By.xpath("//button[text()='Оформить заказ']");
     private static final By ACCOUNT_BUTTON = By.xpath("//p[text()='Личный Кабинет']");
     private static final By HEAD_TEXT = By.xpath(".//h1[text()='Соберите бургер']");
-    private static final By ROLLS_TAB = By.xpath("//span[text()='Булки']");
-    private static final By ROLLS_TEXT = By.xpath("//h2[text()='Булки']");
-    private static final By SOUCES_TAB = By.xpath("//span[text()='Соусы']");
-    private static final By SOUCES_TEXT = By.xpath("//h2[text()='Соусы']");
-    private static final By FILLINGS_TAB = By.xpath("//span[text()='Начинки']");
-    private static final By FILLINGS_TEXT = By.xpath("//h2[text()='Начинки']");
+    private static final By ROLLS_TAB = By.xpath(".//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[1]");
+    private static final By SOUCES_TAB = By.xpath(".//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[2]");
+    private static final By FILLINGS_TAB = By.xpath(".//section[@class='BurgerIngredients_ingredients__1N8v2']/div/div[3]");
+    private static final String ACTIVE_TAB_TEXT = "tab_tab_type_current__2BEPc";
 
     @Step("Нажатие на кнопку 'Войти в аккаунт'")
     public void clickOnLoginButton() {
@@ -55,10 +54,8 @@ public class MainPage {
 
     @Step("Проверка открытия вкладки 'Булки'")
     public boolean isRollsTextOpened() {
-        if (driver.findElement(ROLLS_TEXT).isDisplayed()) {
-            return true;
-        }
-        return false;
+        WebElement element = driver.findElement(ROLLS_TAB);
+        return element.getAttribute("class").contains(ACTIVE_TAB_TEXT);
     }
 
     @Step("Нажатие на вкладку 'Соусы'")
@@ -68,10 +65,8 @@ public class MainPage {
 
     @Step("Проверка открытия вкладки 'Соусы'")
     public boolean isSaucesTextOpened() {
-        if (driver.findElement(SOUCES_TEXT).isDisplayed()) {
-            return true;
-        }
-        return false;
+        WebElement element = driver.findElement(SOUCES_TAB);
+        return element.getAttribute("class").contains(ACTIVE_TAB_TEXT);
     }
 
     @Step("Нажатие на вкладку 'Начинки'")
@@ -81,9 +76,7 @@ public class MainPage {
 
     @Step("Проверка открытия вкладки 'Начинки'")
     public boolean isFillingsTextOpened() {
-        if (driver.findElement(FILLINGS_TEXT).isDisplayed()) {
-            return true;
-        }
-        return false;
+        WebElement element = driver.findElement(FILLINGS_TAB);
+        return element.getAttribute("class").contains(ACTIVE_TAB_TEXT);
     }
 }
